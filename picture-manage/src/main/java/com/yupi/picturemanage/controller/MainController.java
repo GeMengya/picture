6bd372ad6612653496bc425d37bb41bb0f413e50
gemengya <gemengya@hongguaninfo.com>
2025-11-12 09:49:55 +0800

    first commit

---
+++ picture-manage/src/main/java/com/yupi/picturemanage/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.yupi.picturemanage.controller;
+
+import com.yupi.picturemanage.common.BaseResponse;
+import com.yupi.picturemanage.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+
+}
